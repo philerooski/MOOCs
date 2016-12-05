5e4370ac314b4a26f436379206b1078409795447
Phil Snyder <subfivemile@gmail.com>
2016-12-04 17:23:57 -0800

    variable elimination with evidence and ordering to minimize memory use

--- reduce.py
+++ reduce.py
@@ -29,12 +29,14 @@ def parse_graph(graph):
         for f in reverse_scopes[var]:
             concomitant = concomitant.union(scopes[f])
         baggage[var] = concomitant
-    print("baggage", baggage)
     return factors, scopes, reverse_scopes, baggage
 
 def parse_evidence(evidence):
-    split_evidence = [e.split("=") for e in evidence]
-    return {i[0]: int(i[1]) for i in split_evidence}
+    if evidence:
+        split_evidence = [e.split("=") for e in evidence]
+        return {i[0]: int(i[1]) for i in split_evidence}
+    else:
+        return None
 
 def calculate_marginal(event, factors, evidence, scopes, reverse_scopes, baggage):
     """
@@ -43,14 +45,107 @@ def calculate_marginal(event, factors, evidence, scopes, reverse_scopes, baggage
     smallest resulting factor after multiplying all the
     pre-existing factors in which that variable exists
     """
-    #while len(factors) > 1:
-    event_to_sum_out = sorted(baggage, key=lambda x : len(baggage[x]))[0]
+    if evidence:
+        factors = remove_noncompatible_evidence(factors, evidence)
+    baggage.pop(event)
+    while len(baggage):
+        event_to_sum_out = sorted(baggage, key=lambda x : len(baggage[x]))[0]
+        pertinent_factors = reverse_scopes[event_to_sum_out]
+        for k in reverse_scopes:
+            reverse_scopes[k] = reverse_scopes[k].difference(pertinent_factors)
+        product = [factors.pop(p) for p in pertinent_factors]
+        counter = 0
+        while len(product):
+            if len(product) == 1: # sum out
+                f = product.pop()
+                tau = [] # factor table
+                for r in f:
+                    if not len(tau):
+                        r.pop(event_to_sum_out)
+                        tau.append(r)
+                    else:
+                        state = {k: r[k] for k in r if k != event_to_sum_out and k != 'value'}
+                        foundAMatch = False
+                        for r2 in tau:
+                            matches = True
+                            for k in state.keys():
+                                if state[k] != r2[k]:
+                                    matches = False
+                            if matches:
+                                r['value'] += r2['value']
+                                foundAMatch = True
+                                break
+                        if not foundAMatch:
+                            r.pop(event_to_sum_out)
+                            tau.append(r)
+                if len(baggage) == 1:
+                    for f in reverse_scopes[event]:
+                        if f in factors: # singelton potential for event exists
+                            tau = make_new_factor([tau, factors[f]])
+                    tau = renormalize(tau)
+                new_factor_name = "T%s" % counter
+                counter += 1
+                factors[new_factor_name] = tau
+                for var in tau[0].keys():
+                    if var != 'value':
+                        reverse_scopes[var].add(new_factor_name)
+                        scopes[new_factor_name] = set()
+                        scopes[new_factor_name].add(var)
+                baggage.pop(event_to_sum_out)
+            else: # factors left in product
+                product.append(make_new_factor(product))
+    return factors.values()
+
+def make_new_factor(product):
+    f1 = product.pop()
+    f2 = product.pop()
+    shared_vars = set(f1[0].keys()).intersection(f2[0].keys()).difference({'value'})
+    new_factor = []
+    for r in f1:
+        for r2 in f2:
+            matches = True
+            for var in shared_vars:
+                if r[var] != r2[var]:
+                    matches = False
+            if matches:
+                new_factor.append(multiply_rows(r, r2))
+    return new_factor
+
+def remove_noncompatible_evidence(factors, evidence):
+    for f in factors:
+        remove = []
+        for i in range(len(factors[f])):
+            for e in evidence:
+                if e in factors[f][i].keys() and factors[f][i][e] != evidence[e]:
+                    remove.append(i)
+                    break
+        for j in remove[::-1]:
+            factors[f].pop(j)
+    return factors
+
+def renormalize(factor):
+    partition_func = sum([r['value'] for r in factor])
+    for r in factor:
+        r['value'] /= partition_func
+    return factor
+
+def multiply_rows(r, r2):
+    value = r['value']
+    value2 = r2['value']
+    result = {k: r[k] for k in r.keys() if k != 'value'}
+    for var in r2.keys():
+        if var != 'value':
+            result[var] = r2[var]
+    result['value'] = value * value2
+    return result
 
 def main():
     graph, event, evidence = read_args()
     factors, scopes, reverse_scopes, baggage = parse_graph(graph)
     evidence = parse_evidence(evidence)
-    calculate_marginal(event, factors, evidence, scopes, reverse_scopes, baggage)
+    marginal = calculate_marginal(event, factors, evidence, scopes, reverse_scopes, baggage)
+    print(marginal)
+    return marginal
 
 if __name__ == "__main__":
     main()
